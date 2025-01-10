8a721be30c7f3b5cdbdfc9641d4e7c7fb093306e
GradeEight <LightningGamez@hotmail.com>
2025-01-10 21:20:34 +0530

    Add files via upload

---
+++ scriptcredre.js
@@ -0,0 +1,11 @@
+document.getElementById('button1').addEventListener('click', function() {
+    window.location.href = 'https://www.espncricinfo.com/'; // Replace with your desired URL
+});
+
+document.getElementById('button2').addEventListener('click', function() {
+    window.location.href = 'https://krunker.io/'; // Replace with your desired URL
+});
+
+document.getElementById('button3').addEventListener('click', function() {
+    window.location.href = 'https://shellshock.io/'; // Replace with your desired URL
+});
