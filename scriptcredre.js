2a23c9dd25f94906ece729999e82b75de7f11d2c
GradeEight <LightningGamez@hotmail.com>
2025-01-19 16:15:00 +0530

    Update scriptcredre.js

--- scriptcredre.js
+++ scriptcredre.js
@@ -9,3 +9,11 @@ document.getElementById('button2').addEventListener('click', function() {
 document.getElementById('button3').addEventListener('click', function() {
     window.location.href = 'https://www.riotgames.com/en'; // Replace with your desired URL
 });
+
+document.getElementById('button4').addEventListener('click', function() {
+    window.location.href = 'https://shellshock.io/'; // Replace with your desired URL
+});
+
+document.getElementById('button5').addEventListener('click', function() {
+    window.location.href = 'https://assettocorsa.gg/'; // Replace with your desired URL
+});
