8479545b8baecdf0962ac839fa71520be2e73890
GradeEight <LightningGamez@hotmail.com>
2025-02-19 19:04:33 +0530

    Update scriptcredre.js

--- scriptcredre.js
+++ scriptcredre.js
@@ -3,7 +3,7 @@ document.getElementById('button1').addEventListener('click', function() {
 });
 
 document.getElementById('button2').addEventListener('click', function() {
-    window.location.href = 'https://krunker.io/'; // Replace with your desired URL
+    window.location.href = 'hhttps://www.cristianoronaldo.com/'; // Replace with your desired URL
 });
 
 document.getElementById('button3').addEventListener('click', function() {
@@ -11,7 +11,7 @@ document.getElementById('button3').addEventListener('click', function() {
 });
 
 document.getElementById('button4').addEventListener('click', function() {
-    window.location.href = 'https://shellshock.io/'; // Replace with your desired URL
+    window.location.href = 'https://www.epicgames.com/site/en-US/home/'; // Replace with your desired URL
 });
 
 document.getElementById('button5').addEventListener('click', function() {
